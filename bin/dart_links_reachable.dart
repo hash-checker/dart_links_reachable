d5ff240cda3f8b26e5484110a36bba2daed5aa65
fartem <jaman.smlnsk@gmail.com>
2022-10-23 09:57:10 +0300

    2022-10-22 v. 1.0.1: fixed bugs

---
+++ bin/dart_links_reachable.dart
@@ -0,0 +1,5 @@
+import 'package:dart_links_reachable/dart_links_reachable.dart';
+
+void main(List<String> arguments) {
+  check(arguments.first);
+}
--- bin/reachable.dart
+++
@@ -1,5 +0,0 @@
-import 'package:reachable/reachable.dart' as reachable;
-
-void main(List<String> arguments) {
-  reachable.check(arguments.first);
-}
